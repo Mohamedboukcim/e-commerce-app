a06f7d7a41cccb8c2a9f26027c38ba0be1e2af12
Boukcim Mohamed <damoh2237@gmail.com>
2026-04-28 23:32:18 +0100

    frontend : create collection and product pages

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, {createContext} from "react"
+import React, {createContext, useState} from "react"
 import { products } from "../assets/assets"
 
 export const ShopContext = createContext();
@@ -7,9 +7,12 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const [search, setsearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
 
     const value = {
-        products, currency, delivery_fee
+        products, currency, delivery_fee,
+        search, setsearch, showSearch, setShowSearch
     }
 
     return (
